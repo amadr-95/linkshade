94f4eff561539cab9cb9d7fb5cd2613551bb0ece
Amador <amasabcar@outlook.com>
2025-04-25 22:04:30 +0200

    simplify ShortUrlDTO by removing id and not passing the user but only the name

--- src/main/java/com/amador/urlshortener/domain/entities/dto/ShortUrlDTO.java
+++ src/main/java/com/amador/urlshortener/domain/entities/dto/ShortUrlDTO.java
@@ -3,14 +3,12 @@ package com.amador.urlshortener.domain.entities.dto;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Builder
 public record ShortUrlDTO(
-        UUID id,
         String shortenedUrl,
         String originalUrl,
-        UserDTO createdByUser,
+        String createdByUser,
         boolean isPrivate,
         LocalDateTime createdAt,
         LocalDateTime expiresAt,
--- src/main/java/com/amador/urlshortener/services/mapper/ShortUrlMapper.java
+++ src/main/java/com/amador/urlshortener/services/mapper/ShortUrlMapper.java
@@ -2,7 +2,6 @@ package com.amador.urlshortener.services.mapper;
 
 import com.amador.urlshortener.domain.entities.ShortUrl;
 import com.amador.urlshortener.domain.entities.dto.ShortUrlDTO;
-import com.amador.urlshortener.domain.entities.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +9,9 @@ public class ShortUrlMapper {
 
     public ShortUrlDTO toShortUrlDTO(ShortUrl shortUrl) {
         return ShortUrlDTO.builder()
-                .id(shortUrl.getId())
                 .shortenedUrl(shortUrl.getShortenedUrl())
                 .originalUrl(shortUrl.getOriginalUrl())
-                .createdByUser(
-                        UserDTO.builder()
-                                .name(shortUrl.getCreatedByUser().getName())
-                                .build()
-                )
+                .createdByUser(shortUrl.getCreatedByUser().getName())
                 .isPrivate(shortUrl.isPrivate())
                 .createdAt(shortUrl.getCreatedAt())
                 .expiresAt(shortUrl.getExpiresAt())
