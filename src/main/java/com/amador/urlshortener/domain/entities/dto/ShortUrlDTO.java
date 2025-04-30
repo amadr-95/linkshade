639a5f29595b328f02ac3a7a2af44eb1febcd390
Amador <amasabcar@outlook.com>
2025-04-30 21:41:40 +0200

    improve DTOs

--- src/main/java/com/amador/urlshortener/domain/entities/dto/ShortUrlDTO.java
+++ src/main/java/com/amador/urlshortener/domain/entities/dto/ShortUrlDTO.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public record ShortUrlDTO(
         String shortenedUrl,
         String originalUrl,
-        String createdByUser,
+        UserDTO createdByUser,
         boolean isPrivate,
         LocalDateTime createdAt,
         LocalDateTime expiresAt,
--- src/main/java/com/amador/urlshortener/domain/entities/dto/UserDTO.java
+++ src/main/java/com/amador/urlshortener/domain/entities/dto/UserDTO.java
@@ -1,7 +1,12 @@
 package com.amador.urlshortener.domain.entities.dto;
 
+import com.amador.urlshortener.domain.entities.Role;
 import lombok.Builder;
 
 @Builder
-public record UserDTO(Long id, String name) {
+public record UserDTO(
+        Long id,
+        String name,
+        String email,
+        Role role) {
 }
