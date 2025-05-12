1148b0954ebceb35354fead6e577f3c9dae38ac3
Amador <amasabcar@outlook.com>
2025-05-12 15:02:30 +0200

    handle createdByUser logic in the backend

--- src/main/java/com/amador/urlshortener/services/mapper/ShortUrlMapper.java
+++ src/main/java/com/amador/urlshortener/services/mapper/ShortUrlMapper.java
@@ -1,7 +1,6 @@
 package com.amador.urlshortener.services.mapper;
 
 import com.amador.urlshortener.domain.entities.ShortUrl;
-import com.amador.urlshortener.domain.entities.User;
 import com.amador.urlshortener.domain.entities.dto.ShortUrlDTO;
 import com.amador.urlshortener.domain.entities.dto.UserDTO;
 import org.springframework.stereotype.Service;
@@ -10,26 +9,17 @@ import org.springframework.stereotype.Service;
 public class ShortUrlMapper {
 
     public ShortUrlDTO toShortUrlDTO(ShortUrl shortUrl) {
-        UserDTO userDTO = null;
-        User user = shortUrl.getCreatedByUser();
-        if(user != null) {
-            userDTO = toUserDTO(user);
-        }
-
         return ShortUrlDTO.builder()
                 .shortenedUrl(shortUrl.getShortenedUrl())
                 .originalUrl(shortUrl.getOriginalUrl())
-                .createdByUser(userDTO)
+                .createdByUser(UserDTO.builder()
+                        .name(shortUrl.getCreatedByUser() == null ?
+                                "Guest": shortUrl.getCreatedByUser().getName())
+                        .build())
                 .isPrivate(shortUrl.isPrivate())
                 .createdAt(shortUrl.getCreatedAt())
                 .expiresAt(shortUrl.getExpiresAt())
                 .numberOfClicks(shortUrl.getNumberOfClicks())
-                .build();
-    }
-
-    private UserDTO toUserDTO(User user) {
-        return UserDTO.builder()
-                .name(user.getName())
                 .build();
     }
 }
