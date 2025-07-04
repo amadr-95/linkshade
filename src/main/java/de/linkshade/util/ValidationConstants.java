1a3ff0dde3b3c9d482420f58d6dbf929e36c63ef
Amador <amasabcar@outlook.com>
2025-07-04 21:28:59 +0200

    move patterns

--- src/main/java/de/linkshade/util/ValidationConstants.java
+++ src/main/java/de/linkshade/util/ValidationConstants.java
@@ -1,5 +1,7 @@
 package de.linkshade.util;
 
+import java.util.regex.Pattern;
+
 public final class ValidationConstants {
 
     public static final int MIN_URL_EXPIRATION_DAYS = 1;
@@ -7,6 +9,10 @@ public final class ValidationConstants {
     public static final int MIN_URL_LENGTH = 5;
     public static final int MAX_URL_LENGTH = 20;
     public static final String VALID_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public static final Pattern URL_PATTERN = Pattern.compile(
+            "^(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+    public static final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{8,}$");
 
     private ValidationConstants() {
         // Utility class, no instances
