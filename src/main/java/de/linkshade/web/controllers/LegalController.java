4e9901d82f72f47ef18714ba2b58cc18270c10b6
Amador <amasabcar@outlook.com>
2025-12-29 16:44:25 +0100

    refactor legal controller to use route prefix and inject avatar model attribute

--- src/main/java/de/linkshade/web/controllers/LegalController.java
+++ src/main/java/de/linkshade/web/controllers/LegalController.java
@@ -1,18 +1,28 @@
 package de.linkshade.web.controllers;
 
+import de.linkshade.web.controllers.helpers.ModelAttributeHelper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/legal")
+@RequiredArgsConstructor
 public class LegalController {
 
-    @GetMapping("/legal/privacy")
-    public String privacy() {
+    private final ModelAttributeHelper helper;
+
+    @GetMapping("/privacy")
+    public String privacy(Model model) {
+        helper.addAvatarToModel(model);
         return "legal/privacy";
     }
 
-    @GetMapping("/legal/terms")
-    public String terms() {
+    @GetMapping("/terms")
+    public String terms(Model model) {
+        helper.addAvatarToModel(model);
         return "legal/terms";
     }
 }
